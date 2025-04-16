e3d4bfa7a9caaa495272d7f162acff040878127f
Nathanael Buendia <futiax@gmail.com>
2025-04-16 23:30:31 +0200

    Update script.js
    pixel

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ fetch("https://api.ipify.org?format=json")
   .then(res => res.json())
   .then(data => {
     const ip = data.ip;
-    fetch("https://script.google.com/macros/s/TON_ID_DEPLOIEMENT/exec?ip=" + ip)
+    fetch("https://script.google.com/macros/s/AKfycbxyTpgnsMLuKJ2Ko0emkTI742OyZQpSkQa1QvAv8UJ4q4THsH4PC1nEGD9e8AT0h-ZY/exec?ip=" + ip)
       .then(() => console.log("IP envoyée : " + ip));
   });
