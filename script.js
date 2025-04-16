6052fc67040aba09ed619b06d5e42693e2ed112c
Nathanael Buendia <futiax@gmail.com>
2025-04-16 23:37:21 +0200

    Update script.js
    reverse

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-fetch("https://api.ipify.org?format=json")
-  .then(res => res.json())
-  .then(data => {
-    const ip = data.ip;
-    fetch("https://script.google.com/macros/s/AKfycbxyTpgnsMLuKJ2Ko0emkTI742OyZQpSkQa1QvAv8UJ4q4THsH4PC1nEGD9e8AT0h-ZY/exec?ip=" + ip)
-      .then(() => console.log("IP envoyée : " + ip));
-  });
+
